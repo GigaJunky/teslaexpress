5d37c69c3c8d6d6853430e858c818ba7d951df59
Mike Horgan <mhorgan@safewayins.com>
2019-01-08 17:32:25 -0600

    dataArh archive script.

---
+++ bin/dataArc.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+var path = '../data/'
+
+fs.readdir(path, (err, items) => {
+    for (var f of items){
+        let file = path + f
+        fs.stat( file,  (err, stats) => {
+            let fd = file.match(/\d+/g)
+            let fdn = fd ? parseInt( fd[0]) : 0
+            console.log(file, Math.round(stats.ctimeMs) - fdn ,  fdn) //, stats.size )
+        })
+    }
+})
